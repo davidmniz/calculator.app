2f3c9d168e59dd3a38c2f3d44f60dd45ad89f635
davidmniz <davidnizam1@gmail.com>
2020-03-10 10:23:56 +0700

    calculator jadi

--- Calculator.cs
+++ Calculator.cs
@@ -2,10 +2,21 @@ class calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		console.WriteLine("Hasil penambahan : {0} + {1} = {2}", a, b, penambahan(a, b));
+		console.WriteLine("Hasil penambahan : {0} - {1} = {2}", a, b, penambahan(a, b));
+		
+		console.WriteLine("\nTekan sembarang key untuk keluar");
+		console.ReadKey();
 	}
 	static int penambahan (int a, int b)
 	{
 		return a + b;
+	}
+	static int pengurangan (int a, int b)
+	{
+		return a - b;
 	}
 }
